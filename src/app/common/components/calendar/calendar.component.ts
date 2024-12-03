4c28ecdf93e291e6d1920719d3361c4a73bad607
TONHA\tonha <tonhaview@gmail.com>
2024-12-03 21:35:33 +0000

    setting up calandar options

--- src/app/common/components/calendar/calendar.component.ts
+++ src/app/common/components/calendar/calendar.component.ts
@@ -14,18 +14,38 @@ import interactionPlugin from '@fullcalendar/interaction';
   styleUrl: './calendar.component.scss',
 })
 export class CalendarComponent {
+   isMobile = window.innerWidth <= 768;
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: this.isMobile ? 'customButton' : 'customButton',
+      center: 'title',
+      right: this.isMobile ? 'dayGridMonth' : 'prev,next,dayGridMonth,dayGridWeek,dayGridDay'
+    },
+    customButtons: {
+      customButton: {
+        text: 'New',
+        click: function () {
+          alert('Custom Button Clicked!');
+        }
+      }
+    },
     plugins: [dayGridPlugin, interactionPlugin],
-    dateClick: (arg) => this.handleDateClick(arg),
+    // dateClick: (arg) => this.handleDateClick(arg),
     events: [
       { title: 'event 1', date: '2024-12-01' },
       { title: 'event 2', date: '2024-12-02' }
-    ]
+    ],
+    eventClick: (e:any) => {
+      alert(e);
+    }
   };
 
   handleDateClick(arg: any) {
     alert('date click! ' + arg.dateStr)
   }
-  
+
+    toggleWeekends() {
+    this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+  }
  }
